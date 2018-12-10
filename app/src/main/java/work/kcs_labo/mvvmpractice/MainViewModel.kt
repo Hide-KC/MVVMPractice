a92c02db32e8c29ad9028a1b87919ac90443cd63
Hide-KC <kcs.dev.labo@gmail.com>
2018-12-10 20:29:32 +0900

    MainViewModel生成、実装完了

---
+++ app/src/main/java/work/kcs_labo/mvvmpractice/MainViewModel.kt
@@ -0,0 +1,8 @@
+package work.kcs_labo.mvvmpractice
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val inputText = MutableLiveData<String>()
+}
